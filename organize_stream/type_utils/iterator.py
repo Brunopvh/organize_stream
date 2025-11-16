60cf862ca69d1a648a8b4a2ea4ecb024c7e4b9dc
bschaves <brunodasill@gmail.com>
2025-11-16 14:54:55 -0400

    Correções na extração de texto

--- organize_stream/type_utils/iterator.py
+++
@@ -1,4 +0,0 @@
-from __future__ import annotations
-from collections.abc import Iterable, Iterator
-from organize_stream.utils import TableDocuments, ListColumnBody, HeadCell, HeadValues, TableRow
-from sheet_stream import IterRows
